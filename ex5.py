d05760e4e24a5db4b63fbb117ad15ffa3048959f
Joel Pomales <joelpomales@gmail.com>
2015-08-12 23:03:32 -0400

    Modified?

--- ex5.py
+++ ex5.py
@@ -15,3 +15,6 @@ print "His teeth are usually %s depending on the coffee." % my_teeth
 
 # This line is tricky, try to get it exactly right.
 print "If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
+#Why did this comment not go through?
+print "And %d and %d mean crap." % (my_age, my_weight)
+
